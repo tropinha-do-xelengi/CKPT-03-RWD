99f249c9c3400e146d350e451d661eb3f9076ede
NeugeMa <mariana.n.dourado@gmail.com>
2023-05-16 14:10:21 -0300

    inicio da validação

--- cp3.js
+++ cp3.js
@@ -2,3 +2,21 @@ function toggleDarkMode() {
     var body = document.body;
     body.classList.toggle("dark-mode");
 }
+
+const inputUser = document.querySelector("input[type='email']");
+
+inputUser.addEventListener("focus", ()=>{
+    inputUser.setAttribute("style","outline-color:#ff0000;");
+});
+
+
+inputUser.addEventListener("keyup", ()=>{
+    const lblUser = document.querySelector("label[for='idEmail']");
+    if(inputUser.value.length < 5){
+        lblUser.innerHTML = "<span style='color:#ff0000;'>Email (Mínimo de 30 caractéres)</span>"
+        inputUser.setAttribute("style","outline-color:#ff0000;");
+    }else{
+        lblUser.innerHTML = "<span style='color:#00ff00;'>Email</span>"
+        inputUser.setAttribute("style","outline-color:#00ff00;");
+    }
+});
