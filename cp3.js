00f72dee3a10aaba99175a834c786eacde38484f
Femoraes1 <rm98455@fiap.com.br>
2023-05-17 10:56:06 -0300

    Atualização de validação do form 0.2

--- cp3.js
+++ cp3.js
@@ -1,26 +1,49 @@
+//Validação do primeiro nome no form
+const inputNome = document.querySelector("input[id='idNome']");
 
+inputNome.addEventListener("focus", ()=>{
+    if(inputNome.value.length < 3){
+        inputNome.setAttribute("style","outline-color:#ff0000;");
+    } else {
+        inputNome.setAttribute("style","outline-color:#00ff00;");
+    }
+});
 
+inputNome.addEventListener("keyup", ()=>{
+    const lblUser = document.querySelector("label[for='idNome']");
+    if(inputNome.value.length < 3){
+        lblUser.innerHTML = "<span style='color:#ff0000;'>Primeiro Nome (Mínimo de 3 caractéres): </span>";
+        inputNome.setAttribute("style","outline-color:#ff0000;");
+    }else {
+        lblUser.innerHTML = "<span style='color:#00ff00;'>Primeiro Nome: </span>";
+        inputNome.setAttribute("style","outline-color:#00ff00;");
+    }
+});
 
+//Validação do sobrenome no form
+const inputSobrenome = document.querySelector("input[id='idSobrenome']");
+
+inputSobrenome.addEventListener("focus", ()=>{
+    if(inputSobrenome.value.length < 3){
+        inputSobrenome.setAttribute("style","outline-color:#ff0000;");
+    } else {
+        inputSobrenome.setAttribute("style","outline-color:#00ff00;");
+    }
+});
+
+inputSobrenome.addEventListener("keyup", ()=>{
+    const lblUser = document.querySelector("label[for='idSobrenome']");
+    if(inputSobrenome.value.length < 3){
+        lblUser.innerHTML = "<span style='color:#ff0000;'>Segundo Nome (Mínimo de 3 caractéres): </span>";
+        inputSobrenome.setAttribute("style","outline-color:#ff0000;");
+    }else {
+        lblUser.innerHTML = "<span style='color:#00ff00;'>Segundo Nome: </span>";
+        inputSobrenome.setAttribute("style","outline-color:#00ff00;");
+    }
+});
 
 //Configuração do DarkMode
 function toggleDarkMode() {
     var body = document.body;
-    body.classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode");    
 }
-
-const inputUser = document.querySelector("input[type='email']");
-
-inputUser.addEventListener("focus", ()=>{
-    inputUser.setAttribute("style","outline-color:#ff0000;");
-});
-
-inputUser.addEventListener("keyup", ()=>{
-    const lblUser = document.querySelector("label[for='idEmail']");
-    if(inputUser.value.length < 5){
-        lblUser.innerHTML = "<span style='color:#ff0000;'>Email (Mínimo de 30 caractéres)</span>"
-        inputUser.setAttribute("style","outline-color:#ff0000;");
-    }else{
-        lblUser.innerHTML = "<span style='color:#00ff00;'>Email</span>"
-        inputUser.setAttribute("style","outline-color:#00ff00;");
-    }
-});
