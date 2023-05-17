a9f5b6c2a75564273cd4044ea981922f3f111bee
Femoraes1 <rm98455@fiap.com.br>
2023-05-17 11:20:53 -0300

    Att validação nomes + inicio validação email

--- cp3.js
+++ cp3.js
@@ -2,7 +2,7 @@
 const inputNome = document.querySelector("input[id='idNome']");
 
 inputNome.addEventListener("focus", ()=>{
-    if(inputNome.value.length < 3){
+    if(inputNome.value.length < 5){
         inputNome.setAttribute("style","outline-color:#ff0000;");
     } else {
         inputNome.setAttribute("style","outline-color:#00ff00;");
@@ -11,8 +11,8 @@ inputNome.addEventListener("focus", ()=>{
 
 inputNome.addEventListener("keyup", ()=>{
     const lblUser = document.querySelector("label[for='idNome']");
-    if(inputNome.value.length < 3){
-        lblUser.innerHTML = "<span style='color:#ff0000;'>Primeiro Nome (Mínimo de 3 caractéres): </span>";
+    if(inputNome.value.length < 5){
+        lblUser.innerHTML = "<span style='color:#ff0000;'>Primeiro Nome: </span>";
         inputNome.setAttribute("style","outline-color:#ff0000;");
     }else {
         lblUser.innerHTML = "<span style='color:#00ff00;'>Primeiro Nome: </span>";
@@ -20,11 +20,18 @@ inputNome.addEventListener("keyup", ()=>{
     }
 });
 
+inputNome.addEventListener("blur", ()=>{
+    const lblUser = document.querySelector("label[for='idNome']");
+    if (inputNome.value.length <= 5){
+        lblUser.innerHTML = "<span style='color:#000000;'>Primeiro Nome: </span>";
+    }
+});
+
 //Validação do sobrenome no form
 const inputSobrenome = document.querySelector("input[id='idSobrenome']");
 
 inputSobrenome.addEventListener("focus", ()=>{
-    if(inputSobrenome.value.length < 3){
+    if(inputSobrenome.value.length < 5){
         inputSobrenome.setAttribute("style","outline-color:#ff0000;");
     } else {
         inputSobrenome.setAttribute("style","outline-color:#00ff00;");
@@ -33,12 +40,48 @@ inputSobrenome.addEventListener("focus", ()=>{
 
 inputSobrenome.addEventListener("keyup", ()=>{
     const lblUser = document.querySelector("label[for='idSobrenome']");
-    if(inputSobrenome.value.length < 3){
-        lblUser.innerHTML = "<span style='color:#ff0000;'>Segundo Nome (Mínimo de 3 caractéres): </span>";
+    if(inputSobrenome.value.length < 5){
+        lblUser.innerHTML = "<span style='color:#ff0000;'>Segundo Nome: </span>";
         inputSobrenome.setAttribute("style","outline-color:#ff0000;");
     }else {
         lblUser.innerHTML = "<span style='color:#00ff00;'>Segundo Nome: </span>";
         inputSobrenome.setAttribute("style","outline-color:#00ff00;");
+    }
+});
+
+inputSobrenome.addEventListener("blur", ()=>{
+    const lblUser = document.querySelector("label[for='idSobrenome']");
+    if (inputSobrenome.value.length <= 5){
+        lblUser.innerHTML = "<span style='color:#000000;'>Segundo Nome: </span>";
+    }
+});
+
+//Validação do e-mail no form
+const inputEmail = document.querySelector("input[id='idEmail']");
+
+inputEmail.addEventListener("focus", ()=>{
+    if(inputEmail.value.length < 5){
+        inputEmail.setAttribute("style","outline-color:#ff0000;");
+    } else {
+        inputEmail.setAttribute("style","outline-color:#00ff00;");
+    }
+});
+
+inputEmail.addEventListener("keyup", ()=>{
+    const lblUser = document.querySelector("label[for='idEmail']");
+    if(inputEmail.value.length < 5){
+        lblUser.innerHTML = "<span style='color:#ff0000;'>Segundo Nome: </span>";
+        inputEmail.setAttribute("style","outline-color:#ff0000;");
+    }else {
+        lblUser.innerHTML = "<span style='color:#00ff00;'>Segundo Nome: </span>";
+        inputEmail.setAttribute("style","outline-color:#00ff00;");
+    }
+});
+
+inputEmail.addEventListener("blur", ()=>{
+    const lblUser = document.querySelector("label[for='idEmail']");
+    if (inputEmail.value.length <= 5){
+        lblUser.innerHTML = "<span style='color:#000000;'>Segundo Nome: </span>";
     }
 });
 
